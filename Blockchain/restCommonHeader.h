6eaba364894f565f4ed26442154bd22b64326f2a
Devdatta A <8022501+milideva@users.noreply.github.com>
2021-07-15 17:42:49 +0530

    Delete Blockchain directory

--- Blockchain/restCommonHeader.h
+++
@@ -1,9 +0,0 @@
-
-
-#include <string>
-
-string hostStr = "localhost";
-string portStr = "62789";
-string serviceStr = "restblockchain";
-
-string restServerStr = "http://" + hostStr + ":" + portStr + "/" + serviceStr;
